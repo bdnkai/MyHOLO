e04404ca8761e72151e829e58aa757c2862364d2
bdnkai <bdn.kai@gmail.com>
2022-07-29 00:50:09 -0700

    include some additions to app.js to communicate and render Architect Component, Architect model is a placement test

--- src/App.js
+++ src/App.js
@@ -1,29 +1,20 @@
-import React, { useRef } from 'react';
-import { Canvas, useFrame } from '@react-three/fiber';
-import { Box } from '@react-three/drei';
-import './styles.css';
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import Architecture from "./Architecture";
+import { OrbitControls } from "@react-three/drei/";
+import "./css/styles.css";
 
-const Scene = () => {
-	const scene = useRef();
-	useFrame(() => {
-		scene.current.rotation.y += 0.04;
-		scene.current.rotation.x += 0.04;
-		scene.current.rotation.z += 0.04;
-	});
-	return (
-		<group ref={scene}>
-			<Box>
-				<meshLambertMaterial attach='material' color='white' />
-			</Box>
-		</group>
-	);
-};
-
-export default function App() {
-	return (
-		<Canvas>
-			<directionalLight intensity={0.5} />
-			<Scene />
-		</Canvas>
-	);
+const App = () =>{
+  return (
+    <Canvas>
+      <OrbitControls />
+      <ambientLight intensity={0.5} />
+      <directionalLight intensity={0.6} />
+      <Suspense fallback={null}>
+        <Architecture />
+      </Suspense>
+    </Canvas>
+  );
 }
+
+export default App
