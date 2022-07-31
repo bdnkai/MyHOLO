61ad42169fa2b569abe3cfe0ef53798d48ce6515
bdnkai <bdn.kai@gmail.com>
2022-07-31 14:49:52 -0700

    include HoloContext to provide state and actions from HoloReducer, also include empty folder for UserContext which will be similar in concept

---
+++ src/global_context/holo-context/HoloContext.js
@@ -0,0 +1,5 @@
+import { createContext } from "react";
+
+const HoloContext = createContext();
+
+export default HoloContext
---
+++ src/global_context/holo-context/HoloState.js
@@ -0,0 +1,29 @@
+import {useContext, useState, useMemo} from "react";
+import HoloContext from "./HoloContext";
+
+const initialState = {
+   holo_name: 'Lorem',
+   health: 100,
+   isAlive: true,
+   happiness: 25,
+   hunger: 25,
+   holo_coin: 10000,
+   created_at: '', 
+}
+
+
+const HoloState = (components) =>{
+   const [myHolo, setHolo] = useState(initialState)
+
+
+   return(
+      <HoloContext.Provider value={{myHolo, setHolo}}>
+         {components.children}
+      </HoloContext.Provider>
+   )
+
+
+
+}
+
+export default HoloState;
