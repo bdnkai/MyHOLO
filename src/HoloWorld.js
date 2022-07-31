c01af4fb1f5d6287dd167208dae6c8214edfcc36
bdnkai <bdn.kai@gmail.com>
2022-07-31 06:08:47 -0700

    include buttons for Holo, and a place to render Holo stats

--- src/HoloWorld.js
+++ src/HoloWorld.js
@@ -7,16 +7,16 @@ import "./css/reset.css"
 import "./css/styles.css";
 
 const initialState = {
-   holo_name: 'bdbdbdbd',
+   holo_name: 'RealName',
    health: 100,
-   boredom: 0,
-   hunger: 0,
+   happiness: 25,
+   hunger: 25,
    created_at: null, 
 }
 
 const HoloWorld = () => {
    const [myHolo, setHolo] = useState(initialState)
-   // const [myHolo, dispatch] = useReducer(myHolo, initialState)
+
 
    
    return(
@@ -28,18 +28,28 @@ const HoloWorld = () => {
                   <OrbitControls />
                   <ambientLight intensity={0} />
                   <directionalLight intensity={0.5} />
-                <Suspense fallback={null}>
-                   <Architecture />
-                 </Suspense>
+                  <Suspense fallback={null}>
+                      <Architecture />
+                  </Suspense>
                </Canvas>
              </div>
-             <div className="overlay-display" >
-               <div className="holo-attributes">
-                  <span>Name: {myHolo.holo_name}</span>
-                  <span>Health: {myHolo.health}</span>
-                  <span>Boredom: {myHolo.boredom}</span>
-                  <span>Hunger: {myHolo.hunger} </span>
+             <div className="holo-buttons">
+                  <button className="open-attributes">X</button>
                </div>
+             <div className="overlay-display" >
+               
+               <div className="holo-attributes" style={{display: ''}}>
+                  <span className="holo-name"> {myHolo.holo_name}</span>
+                  <span className="holo-health"> Health: {myHolo.health} </span>
+                  <div className='health-bar'></div>
+                  <span className="holo-happiness"> Happiness: {myHolo.happiness} </span>
+                  <div className='happy-bar'></div>
+                  <span className="holo-hunger"> Hunger: {myHolo.hunger} </span>
+                  <div className='hungry-bar'></div>
+                  <span className="holo-coin"> Holo Coin : {myHolo.health} </span>
+                  <div className='coin-bar'></div>
+               </div>
+
              </div>
            </div>
          </div>
